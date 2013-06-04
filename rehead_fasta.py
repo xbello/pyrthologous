722511f5730014f656ccfc9778911ab9f46726be
Xabi Bello <xbello@gmail.com>
2013-06-04 12:08:47 +0200

    Added fasta name normalization

--- rehead_fasta.py
+++ rehead_fasta.py
@@ -8,7 +8,22 @@ def process_file(fasta="", equivalences=""):
     if not os.path.isfile(fasta):
         return False
 
+    new_fasta = list(os.path.split(os.path.abspath(fasta)))
+    new_fasta[1] = new_fasta[1].split(".")[0] + "_NORM.fas"
+    new_fasta_fn = os.path.join(new_fasta[0], new_fasta[1])
+    new_fasta_fp = open(new_fasta_fn, "w")
+
     equiv_file = open(equivalences, "w") 
+
+    n_seq = 0
+    for rec in SeqIO.parse(fasta, "fasta"):
+        new_fasta_fp.write(">{0}\n".format(n_seq))
+        equiv_file.write("{0},{1}\n".format(rec.name, n_seq))
+        n_seq += 1
+        new_fasta_fp.write("{0}\n".format(rec.seq))
+
+    equiv_file.close()
+    new_fasta_fp.close()
     return True
 
 if __name__ == "__main__":
