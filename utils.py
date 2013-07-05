8a5e9be951ccd701ae0c7c458dd6372f41328379
Xabi Bello <xbello@gmail.com>
2013-07-05 09:19:45 +0200

    Added utils.py and extracted common method

---
+++ utils.py
@@ -0,0 +1,14 @@
+from config import BASE_PATH
+
+def make_reciprocals(src_dir):
+    """String -> List
+
+    Where String is the name of the leaf which contains all genomes and
+    List is the list of permutations to be generated
+    """
+    genome_src = os.path.join(BASE_PATH, src_dir)
+    reciprocals = [x for x in os.listdir(genome_src) if x.endswith("fas")]
+    permuts = [x for x in itertools.combinations(reciprocals, 2)]
+
+    return permuts
+
