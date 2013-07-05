45d8a7141f5a985c2707911d1d9e6abf48403a15
Xabi Bello <xbello@gmail.com>
2013-07-05 12:09:02 +0200

    Added a couple of options to config.py

--- config.py
+++ config.py
@@ -1,1 +1,19 @@
 SEP = ";"
+MUSCLE = "/home/xbello/bin/muscle"
+
+BASE_PATH = "/opt/space/data/Parasitos/secuencias"
+LEAF_PATHS = ["Babesia", "Giardia", "Theileria", "Toxo"]
+
+ORGANISMS = {"Bv": "Babesia_bovis",
+    "Bm": "Babesia_microti",
+    "GA": "Giardia_A",
+    "GB": "Giardia_B",
+    "GE": "Giardia_E",
+    "Tp": "Theileria_parva",
+    "Ta": "Theileria_annulata",
+    "TG": "Toxo_GT",
+    "TM": "Toxo_ME",
+    "TV": "Toxo_VE"}
+
+OUTPUT = "outputs"
+
