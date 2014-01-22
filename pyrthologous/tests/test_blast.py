d34e3d8c6f6d3a384a963fc18807955b82aa41d6
Xabi Bello <xbello@gmail.com>
2014-01-22 10:20:32 +0100

    Test adapted to check only first blastp line of output.

--- pyrthologous/tests/test_blast.py
+++ pyrthologous/tests/test_blast.py
@@ -15,7 +15,7 @@ class testBlast(TestCase):
             os.path.join(self.tgt_path, "prot2.fasta"))
 
         self.blast_output = "Protein_query\tProt2\t99.33\t300\t0\t2" +\
-            "\t122\t420\t2\t300\t0.0\t 626\n"
+            "\t122\t420\t2\t300\t0.0\t 626"
 
     def tearDown(self):
         for filep in os.listdir(self.tgt_path):
@@ -23,12 +23,14 @@ class testBlast(TestCase):
                 os.unlink(os.path.join(self.tgt_path, filep))
 
     def test_blastp(self):
+        """Test blastp output: shown blastp output vs expected output."""
         db = blast.make_blast_db(self.prots,
                                  os.path.join(self.tgt_path, "prots"))
 
-        self.assertEqual(
-            blast.blastp(self.seq1, db),
-            (self.blast_output, ''))
+        stdout, stderr = blast.blastp(self.seq1, db)
+
+        self.assertEqual(stdout.split("\n")[0], self.blast_output)
+        self.assertEqual(stderr, "")
 
     def test_make_blast_db(self):
         self.assertEqual(
