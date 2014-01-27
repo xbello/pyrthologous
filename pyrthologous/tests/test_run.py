ee43bbb0d3eb401aa7a0b96a2b1ce406d6ffd82a
Xabi Bello <xbello@gmail.com>
2014-01-27 18:10:16 +0100

    Removed unused tests

--- pyrthologous/tests/test_run.py
+++ pyrthologous/tests/test_run.py
@@ -4,12 +4,4 @@ from unittest import TestCase
 from .. import run
 
 class testRun(TestCase):
-    def test_config_parse(self):
-        self.assertEqual(
-	    run.config_parse(os.path.join("tests", "config_test.cfg")),
-	    {"BLAST":
-                {"blast":
-                     "C:\\Program files\\NCBI\\blast-2.2.28+\\bin",
-		 "blastp":
-                     "C:\\Program files\\NCBI\\blast-2.2.28+\\bin\\blastp.exe"}
-		})
+    pass
