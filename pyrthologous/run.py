24ac493b5fe2e9b859017fdae766467deb484294
Xabi Bello <xbello@gmail.com>
2014-01-28 20:10:11 +0100

    Step added to run script.

--- pyrthologous/run.py
+++ pyrthologous/run.py
@@ -2,6 +2,7 @@
 
 import os
 
+import blast
 from .config import *
 from .prepare import translate_fasta
 
@@ -11,14 +12,21 @@ if __name__ == "__main__":
         # Test if the files exists
         for specie in pair:
             if not os.path.isfile(os.path.join(BASE_PATH, GENOMES, specie)):
-                print os.path.join(BASE_PATH, GENOMES, specie)
                 raise IOError("File {0} doesn't exist".format(specie))
 
-            # Translate both genomes
-            translate_fasta(os.path.join(BASE_PATH, GENOMES, specie),
-                            output_path=os.path.join(BASE_PATH, OUTPUT))
+            # Translate both genomes only if they doesn't exist
+            this_genome = os.path.join(BASE_PATH, GENOMES, specie)
+            if not os.path.isfile(this_genome):
+                translate_fasta(os.path.join(BASE_PATH, GENOMES, specie),
+                                output_path=os.dirname(this_genome))
 
         # TODO: Reciprocal blast'em
+        abs_paths = [os.path.join(BASE_PATH, OUTPUT, x) for x in pair]
+
+        print [x for x in blast.reciprocal_blastp(abs_paths)]
+        break
+
+
         # TODO: Select best matches and write'm out
         # TODO: Align the matches
         # TODO: Un-translate the sequences
